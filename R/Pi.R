c7e31988b577f5ef440746ff61ec50fe99553218
apple <liu.xu@sufe.edu>
2019-05-05 11:04:26 +0800

    updated

--- R/Pi.R
+++ R/Pi.R
@@ -3,14 +3,23 @@ Pi <- function(n=100){
   
   if (.Platform$OS.type=="windows") {
     platform <- "windows"
+    dlyname <- "libgmp-10.dll"
+    dynpath <- system.file("exec",dlyname,package="PI")
+    dyn.load(dynpath)
   }
   else{
-    if(substr(R.version$os,1,6)=="darwin") platform <- "mac"
+    if(substr(R.version$os,1,6)=="darwin"){
+      platform <- "mac"
+      dlyname <- "libgmp.10.dylib"
+      dynpath <- system.file("exec",dlyname,package="PI")
+      dyn.load(dynpath)
+    } 
     else if(R.version$os=="linux-gnu") platform <- "linux"
     else stop("\nThis package not available for OS ",R.version$os, "\n It only runs currently on Windows OS, Maco OS, or Linux OS.")
   }
   progname <- paste("PIexe-",platform,".exe",sep="")
   progpath <- system.file("exec",progname,package="PI")
+  
   pi_str0 = paste0(rep("0",n+4),collapse = "")
   pi_str <- system2(progpath,args=c(as.character(n),pi_str0),stdout=TRUE)
   pi <- paste0("3",pi_str,collapse = "")
